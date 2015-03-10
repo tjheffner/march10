7f0b1d9a7be2255da05a2e0b25eb4fc1aeccd420
Tanner Heffner & John Franti <student@epicodus.com>
2015-03-10 11:31:13 -0700

    Add functionality to twig pages

--- anagram/app/app.php
+++ anagram/app/app.php
@@ -14,7 +14,11 @@
         return $app['twig']->render("input_form.twig");
     });
 
-    //other pages go here
+    $app->get("/anagram_check", function() use($app) {
+        $input_words = new Anagram;
+        $match = $input_words->compareWords($_GET['input1'], $_GET['input2']);
+        return $app['twig']->render("anagram_check.twig", array('answer' => $match));
+    });
 
     return $app;
 
