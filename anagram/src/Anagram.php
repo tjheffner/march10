265758d0fc39b4d533f686a7d6d2d5a7f387904b
Tanner Heffner & John Franti <student@epicodus.com>
2015-03-10 11:46:32 -0700

    Complete stage1 web functionality

--- anagram/src/Anagram.php
+++ anagram/src/Anagram.php
@@ -5,19 +5,21 @@ class Anagram
 
     function compareWords($input1, $input2)
     {
-        $word1 = str_split($input1);
-        $word2 = str_split($input2);
+        $input2u = strtoupper($input2);
+        $input1u = strtoupper($input1);
+        $word1 = str_split($input1u);
+        $word2 = str_split($input2u);
         $array1 = sort($word1);
         $array2 = sort($word2);
 
         if ($input1 == $input2) {
-            return true;
+            return $input2u." is an anagram of ".$input1u;
         }
         elseif ($word1 !== $word2) {
-            return false;
+            return $input2u." is not an anagram of ".$input1u;
         }
         elseif ($array1 == $array2) {
-            return true;
+            return $input2u." is an anagram of ".$input1u;
         };
     }
 
