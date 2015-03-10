c06794fc52ce005e8d86cec110e9bd0b02791aa7
Tanner Heffner & John Franti <student@epicodus.com>
2015-03-10 10:02:55 -0700

    Fix directory requirements.

--- anagram/app/app.php
+++ anagram/app/app.php
@@ -1,6 +1,6 @@
 <?php
-    require_once = __DIR__."/../vendor/autoload.php";
-    require_once = __DIR__."/../src/Anagram.php";
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Anagram.php";
 
     $app = new Silex\Application();
 
