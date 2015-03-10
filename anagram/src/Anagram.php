30d22e4eb641d6a78e27cef66d49bfbe2aa3a1c5
Tanner Heffner & John Franti <student@epicodus.com>
2015-03-10 11:01:28 -0700

    pass test 3

--- anagram/src/Anagram.php
+++ anagram/src/Anagram.php
@@ -5,11 +5,19 @@ class Anagram
 
     function compareWords($input1, $input2)
     {
+        $word1 = str_split($input1);
+        $word2 = str_split($input2);
+        $array1 = sort($word1);
+        $array2 = sort($word2);
+
         if ($input1 == $input2) {
             return true;
         }
-        elseif ($input1 !== $input2) {
+        elseif ($word1 !== $word2) {
             return false;
+        }
+        elseif ($array1 == $array2) {
+            return true;
         };
     }
 
